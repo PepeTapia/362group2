5494b0a295d93cfda342e42cf0e09624ad0067a5
brandon-hoang-le <55907638+brandon-hoang-le@users.noreply.github.com>
2021-09-15 21:21:17 -0700

    Add files via upload

---
+++ php/src/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"></link>
+    <script src="../Resources/NavBar/navbar_script.js"></script>
+  </head>
+  <body onload="getLogsStatus()">
+    <!-- Top Nav Bar -->
+    <div class="topnav">
+        <a>Cloud_Site</a>
+    </div>
+    <!-- Login Element -->
+    <div style="padding-left:16px">
+        <div class="display-middle" style="white-space:nowrap;">
+            <div class = "headline-animate-opacity"  >
+                <div class="headline-section">
+                    <img class="plane-icon" src="/Resources/images/plane_attempt.png">
+                    <p>
+                        Sample Text<br> GoBin File Storage
+                    </p>
+                    <a  id="loginout-link" href="#" style="text-decoration: none;" onclick="toLogin()">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+  </body>
+</html>
