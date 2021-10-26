8803f7a8d893619a120db878839b95703435c171
brandon-hoang-le <55907638+brandon-hoang-le@users.noreply.github.com>
2021-10-25 20:14:53 -0700

    Updated directories to login system

--- titanbin/index.php
+++ titanbin/index.php
@@ -2,33 +2,29 @@
 <?php
 
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
-	case '/echo.php':
-		include './resources/test/echo.php';
-		break;
-    	case '/':
-		include 'intro.php';
-		break;
+    case '/':
+        require 'intro.php';
+        break;
 	case '/intro.php':
 		include 'intro.php';
 		break;
 	case '/login.php':	 
-		include 'login.php';
+		include './login/login.php';
 		break;
 	case '/signup.php':
-		include 'signup.php';
+		include './login/signup.php';
 		break;
 	case '/home.php':
 		include 'home.php';
 		break;
-	case '/files.php':
-		include 'files.php';
-		break;
 	case '/trash.php':
 		include 'trash.php';
 		break;
+	case '/files.php':
+		include 'files.php';
+		break;
     default:
         http_response_code(404);
-        exit('Not Found');
 }
 
 // switch ($_SERVER['REQUEST_URI']) {
