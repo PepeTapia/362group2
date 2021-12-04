5512e8b29ab29440d5249122832897b857f6da88
brandon-hoang-le <55907638+brandon-hoang-le@users.noreply.github.com>
2021-12-04 00:26:54 -0800

    Add file extension to get_object

--- api/get.php
+++ api/get.php
@@ -9,7 +9,7 @@ $storage = new googleStorage();
 $storage->set_bucket("titanbin-api-". $bucketName);
 
 if (isset($_GET['bucket']) && isset($_GET['filename'])){
-    print_r($storage->get_object($_GET['filename']));
+    print_r($storage->get_object($_GET['filename'] . ".json"));
 } else if (!isset($_GET['filename'])){
     print_r($storage->get_all_objects());
 } 
